fc7d7744b65053003cedea63ba5e964d1f02d5d2
juan <x6juan6x@gmail.com>
2026-04-22 19:41:36 +0200

    feat: Setup bootstrap project

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import Fastify from "fastify";
+
+const app = Fastify({ logger: true });
+
+const BACKEND_PORT = process.env.BACKEND_PORT || 3000;
+
+async function main() {
+  await app.listen({ port: Number(BACKEND_PORT), host: "0.0.0.0" })
+    .then(() => console.log(`Server running on port ${BACKEND_PORT}`));
+}
+
+main();
